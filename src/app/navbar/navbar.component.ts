a828b14a76a8c908b3d2320b0e29a45008c8acfd
Jeeda2004 <jk8835@g.rit.edu>
2024-11-22 23:42:49 +0400

    Logout functionality completed

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { StudentService } from '../student.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  constructor(
+    private studentService: StudentService
+  ) {}
+  
+  logout(): void {
+    this.studentService.logout(); // Assuming this returns the student object
+  }
 }
